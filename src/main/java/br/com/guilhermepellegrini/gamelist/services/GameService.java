438046527b6daeec4a63c092c3b6b44b6e2ed3d4
GuilhermePellegrini <pellegrini458@gmail.com>
2023-07-11 23:54:03 -0300

    GameController Update

--- src/main/java/br/com/guilhermepellegrini/gamelist/services/GameService.java
+++ src/main/java/br/com/guilhermepellegrini/gamelist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.guilhermepellegrini.gamelist.dto.GameDTO;
 import br.com.guilhermepellegrini.gamelist.dto.GameMinDTO;
 import br.com.guilhermepellegrini.gamelist.entities.Game;
 import br.com.guilhermepellegrini.gamelist.repositories.GameRepository;
@@ -15,9 +17,17 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
+	
 }
